ef1ae50c1de3960e0a33667efe172cc6e3c27b9f
„Csongrádi <cs.tomi0307@gmail.com>
2026-04-17 16:13:15 +0200

    2. Törésteszt

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,43 @@
+from math import comb
 from pathlib import Path
 
+def min_num_of_drops(N, H):
+    if H == 0:
+        return 0
+    
+    # Próbák száma
+    m = 0
+
+    while True:
+        m += 1
+
+        # Kiszámoljuk, hány szintet tudunk tesztelni m próbával
+        total = 0
+        for i in range(1, N + 1):
+            total += comb(m, i)
+            if total >= H:
+                break
+
+        if total >= H:
+            return m
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    data = Path("input.txt").read_text(encoding="utf-8").strip().splitlines()
+
+    for line in data:
+        line = line.strip()
+        if not line:
+            continue
+        
+        # Adatok
+        parts = line.split(",")
+        if len(parts) != 2:
+            continue
+        
+        N, H = map(int, parts)
+
+        result = min_num_of_drops(N, H)
+        print(result)
 
 
 if __name__ == "__main__":
